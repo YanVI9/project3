af8074669b8fc43101630dbd93b548184a9ed942
Yan Itskevich <yan.itskevich@gmail.com>
2018-09-24 05:54:55 +0300

    Произведены изменения
    Пришли замечания - исправлены

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function openWindow() {
+    document.getElementById("window").style.display = 'flex';
+    document.getElementById("window").style.opacity = '1';
+}
+
+function closeWindow() {
+    document.getElementById("window").style.display = 'none';
+    document.getElementById("window").style.opacity = '0';
+}
